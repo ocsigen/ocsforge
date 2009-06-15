123694d47365b81180f3b45be2a7eb4129d053a9
raphlalou <raphlalou@3ad814b1-d9dd-4a2c-a3f2-008bc9d20e61>
2009-06-15 09:40:15 +0200

    Unnamed patch
    Ignore-this: 2a6194e1e4e6e9a71892251c1347dfce
    
    git-svn-id: svn://svn.forge.ocamlcore.org/svn/ocsforge@3 3ad814b1-d9dd-4a2c-a3f2-008bc9d20e61
    
    darcs-hash:20090615074015-eefae-1472cda0a8536caaacc1deabed61c72b07d3353f

--- ocsforge_createdb.sql
+++ ocsforge_createdb.sql
@@ -45,8 +45,8 @@ CREATE TABLE ocsforge_tasks (
     edit_version text NOT NULL,
 
     length interval,
-    progress integer DEFAULT NULL CHECK (progress >=0 AND progress <=100),
-    importance integer DEFAULT 20 NOT NULL CHECK (importance >= 0 AND importance <= 100),
+    progress integer DEFAULT NULL, 
+    importance integer DEFAULT 20 NOT NULL, 
     deadline_time timestamp,
     deadline_version text,
     kind text DEFAULT 'MISC' NOT NULL,
--- ocsforge_sql.ml
+++ ocsforge_sql.ml
@@ -18,192 +18,7 @@
  *)
 
 let (>>=) = Lwt.bind
-
-
-(* {2 Type conversion for database IO} *)
-module Types =
-struct
-
-  (** {3 Semi-abstract type for a task, a right tag and a task history} *)
-  type task_arg = [ `Task ]
-  type task = task_arg Opaque.int32_t
-  type right_area_arg = [ `Right_area ]
-  type right_area = right_area_arg Opaque.int32_t
-  type task_history_arg = [ `Task_history ]
-  type task_history = task_history_arg Opaque.int32_t
-
-
-  (** Needed intermediary types. *)
-  exception Not_a_percent
-  type percent = int32 (*TODO : hide type*)
-  let percent_of_int32 (n : int32) =
-    if ( let m = Int32.to_int n in 100>=m && m>=0 )
-    then (n : percent)
-    else raise Not_a_percent
-  let int32_of_percent (n : percent) = (n : int32) 
-
-
-  (** {3 For right tags : right management for project tree} *)
-  type right_area_info = {
-    r_id      : right_area;
-    r_forum   : Forum_sql.Types.forum;
-    r_version : string;
-  }
-
-  let right_area_of_sql (u : int32) = (Opaque.int32_t u : right_area)
-  let sql_of_right_area (u : right_area) = Opaque.t_int32 u
-
-  let right_area_of_sql_option (u : int32 option) = 
-    (Opaque.int32_t_option u : right_area option)
-  let sql_of_right_area_option (u : right_area option) = Opaque.t_int32_option u
-
-  let string_of_right_area i = Int32.to_string (sql_of_right_area i)
-  let right_area_of_string s = (Opaque.int32_t (Int32.of_string s) : right_area)
-
-  type raw_right_area_info =
-      (int32 * int32)
-
-  let get_right_area_info (id, forum_id, ver) =
-    {
-      r_id      = right_area_of_sql id;
-      r_forum   = Forum_sql.Types.forum_of_sql forum_id;
-      r_version = ver
-    }
-
-
-  (** {3 For tasks} *)
-  type task_info = {
-    t_id        : task;
-    t_parent    : task;
-
-    t_message : Forum_sql.Types.message;
-    
-    t_edit_author  : User_sql.Types.userid;
-    t_edit_time    : CalendarLib.Calendar.t;
-    t_edit_version : string;
-
-    t_length           : Calendar.Period.t option;
-    t_progress         : percent option;
-    t_importance       : percent;
-    t_deadline_time    : CalendarLib.Calendar.t option;
-    t_deadline_version : string option;
-    t_kind             : string;
-
-    t_area             : right_area;
-    t_area_inheritance : right_area;
-  }
-    
-
-
-  let task_of_sql (u : int32) = (Opaque.int32_t u : task)
-  let sql_of_task (u : task) = Opaque.t_int32 u
-
-  let task_of_sql_option (u : int32 option) = 
-    (Opaque.int32_t_option u : task option)
-  let sql_of_task_option (u : task option) = Opaque.t_int32_option u
-
-  let string_of_task i = Int32.to_string (sql_of_task i)
-  let task_of_string s = (Opaque.int32_t (Int32.of_string s) : task)
-
-  type raw_task_info =
-      (int32 * int32 *
-       int32 *
-       int32 * CalendarLib.Calendar.t * string *
-       CalendarLib.Period.t option * int32 option * int32
-         * CalendarLib.Calendar.t option * string option * string *
-       int32 * int32)
-
-  let get_task_info
-      (id,  parent_id,
-       message,
-       edit_author, edit_time, edit_version,
-       length,  progress,  importance,  deadline_time, deadline_version,  kind,
-       area,  area_inheritance)
-      = 
-    {
-      t_id        = task_of_sql id;
-      t_parent_id = task_of_sql parent_id;
-
-      t_message = Forum_sql.Types.message_of_sql message;
-
-      t_edit_author  = User_sql.Types.userid_from_sql editor_id;
-      t_edit_time    = datetime_edit;
-      t_edit_version = edit_version;
-
-      t_length           = length;
-      t_progress         = ForgeLang.aplly_on_opted percent_of_int32 progress;
-      t_importance       = percent_of_int32 importance;
-      t_deadline_time    = deadline_time;
-      t_deadline_version = deadline_version;
-      t_kind             = task_kind_of_string kind;
-
-      t_area             = right_area_of_sql right_tag;
-      t_area_inheritance = right_area_of_sql right_inheritance;
-    }
-
-
-  (** {3 For tasks history} *)
-  type task_history_info = {
-    th_id        : task;
-    th_parent_id : task;
-
-    th_edit_author  : User_sql.Types.userid;
-    th_edit_time    : CalendarLib.Calendar.t;
-    th_edit_version : string;
-
-    th_length           : Calendar.Period.t option;
-    th_progress         : percent option;
-    th_importance       : percent;
-    th_deadline_time    : CalendarLib.Calendar.t option;
-    th_deadline_version : string option;
-    th_kind             : string;
-
-    th_area             : right_area;
-    th_area_inheritance : right_area;
-  }
-
-  let task_history_of_sql (u : int32) = (Opaque.int32_t u : task_history)
-  let sql_of_task_history (u : task_history) = Opaque.t_int32 u
-
-  let task_history_of_sql_option (u : int32 option) = 
-    (Opaque.int32_t_option u : task_history option)
-  let sql_of_task_history_option (u : task_history option) = Opaque.t_int32_option u
-
-  let string_of_task_history i = Int32.to_string (sql_of_task i)
-  let task_history_of_string s = (Opaque.int32_t (Int32.of_string s) : task)
-
-  type raw_task_history_info =
-      (int32 * int32 *
-       int32 * CalendarLib.Calendar.t * string *
-       CalendarLib.Period.t option * int32 option * int32
-         * CalendarLib.Calendar.t option * string option * string *
-       int32 * int32)
-
-  let get_task_history_info
-      (id, parent,
-       editor, time, edit_version,
-       length, progress, importance, deadline_time, deadline_ver, kind, 
-       right_zone, right_inheritance) =
-    {
-    th_id        = task_history_of_sql id ;
-    th_parent_id = task_of_sql parent ;
-
-    th_edit_author  = User_sql.Types.userid_from_sql editor ;
-    th_edit_time    = datetime ;
-    th_edit_version = edit_version ;
-
-    th_length           = length ;
-    th_progress         = ForgeLang.apply_on_opted percent_of_int32 progress ;
-    th_importance       = percent_of_int32 importance ;
-    th_deadline_time    = deadline_time ;
-    th_deadline_version = deadline_ver ;
-    th_kind             = kind_of_string kind ;
-
-    th_area             = right_area_of_sql right_zone ;
-    th_area_inheritance = right_area_of_sql right_inheritance ;
-  }
-
-end
+module Types = Ocsforge_types
 
 
 
@@ -224,18 +39,20 @@ end
 
 let new_task 
       ~parent ~message ~creator
-      ?length
-      ?(progress   = Defaults.progress  )
+      ?length ?progress
       ?(importance = Defaults.importance)
       ?deadline_time ?deadline_version
       ?(kind       = Defaults.kind      )
-      ~area
+      ~area ?area_inheritance
       () =
   let parent_id  = Types.sql_of_task parent in
   let message_id = Forum_sql.Types.sql_of_message message in
   let creator_id = User_sql.Types.sql_from_userid creator in
   let area_id    = Types.sql_of_right_area area in
   let now        = CalendarLib.Calendar.now () in
+  let area_inh_id = match area_inheritance with
+    | None -> area_id | Some a -> Types.sql_of_right_area a
+  in
     Sql.full_transaction_block
       (fun db ->
          PGSQL(db)
@@ -252,7 +69,7 @@ let new_task
             VALUES ($parent, $message, $creator_id, $now, $version, \
                     $length, $progress, $importance, \
                     $deadline_time, $deadline_version, $kind, \
-                    $area_id, $area_id)"
+                    $area_id, $area_inh_id)"
        >>= fun () -> serial4 db "ocsforge_task_id_seq"
        >>= fun i -> Lwt.return (Types.task_of_sql i))
 
@@ -272,7 +89,7 @@ let new_area ~forum () =
 
 (** {5 getters for tasks} *)
 
-(*
+
 let get_task_by_id ?db ~task_id () =
   let task_id = Types.sql_of_task task_id in
   let f db =
@@ -336,7 +153,7 @@ let get_tasks_by_editor ?db ~editor () =
   in match db with
     | None    -> Sql.full_transaction_block f
     | Some db -> f db
- *)
+
 (*TODO : more getter by attributes (version, time, progress...*)
 
 (** {5 getters for area} *)
@@ -348,7 +165,7 @@ let get_area_inheritance ~task_id () =
          "SELECT area_inheritance
           FROM ocsforge_tasks
           WHERE id = $parent_id")
-(*
+
 let get_area_by_id ~area_id () =
   let area = Types.sql_of_right_area area_id in
     Lwt_pool.use Sql.pool
@@ -358,7 +175,7 @@ let get_area_by_id ~area_id () =
             FROM ocsforge_right_areas
             WHERE id = $area"
        >>= fun r -> Lwt.return (Types.get_right_area_info r))
- *)
+
 
 (** {3 history management : to record changes } *)
 
