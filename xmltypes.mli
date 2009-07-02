ee545d623e01d9b64e184dfcba356dad6b09f0ac
jhgranarolo <jhgranarolo@3ad814b1-d9dd-4a2c-a3f2-008bc9d20e61>
2009-07-02 14:51:03 +0200

    Ajout du module pour les types xml
    Ignore-this: 6f71bbc3294e52d76329f05f71d7737
    
    git-svn-id: svn://svn.forge.ocamlcore.org/svn/ocsforge@58 3ad814b1-d9dd-4a2c-a3f2-008bc9d20e61
    
    darcs-hash:20090702125103-c812b-252673c5c2a8e5b7ce385c482830154e2d76f70a

---
+++ xmltypes.mli
@@ -0,0 +1,22 @@
+type patch_infos = {xml_author:string;
+		    xml_email:string;
+		    xml_date:string;
+		    xml_local_date: string;
+		    xml_inverted: string;
+		    xml_hash: string}
+
+type change = 
+  | Add_dir of string
+  | Rm_dir of string
+  | Add_file of string
+  | Rm_file of string
+  | Modify_file of string
+  | Move_file of (string*string)
+    
+type xml_patch = {xml_infos:patch_infos;
+		  xml_name:string;
+		  xml_comment:string;
+		  xml_tree_changes:change list}
+
+
+type changeLog = xml_patch list
