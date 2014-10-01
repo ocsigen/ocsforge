5d8eccd1d6e6985080995e7d6f0b02781cfe77f9
hhugo <hugo.heuzard@gmail.com>
2014-10-01 02:31:30 +0200

    Update .jenkins.sh

--- .jenkins.sh
+++ .jenkins.sh
@@ -2,7 +2,6 @@
 opam pin add --no-action ocsforge .
 opam pin add --no-action ocsigenserver 'https://github.com/ocsigen/ocsigenserver.git#master'
 opam pin add --no-action eliom 'https://github.com/ocsigen/eliom.git#master'
-opam pin add --no-action js_of_ocaml 'https://github.com/ocsigen/js_of_ocaml.git#master'
 EDITOR="sed -i s/0.6.1/dev/" opam pin add --no-action macaque 'https://github.com/ocsigen/macaque.git#master'
 opam pin add --no-action ocsimore 'https://github.com/ocsigen/ocsimore.git#master'
 opam install --deps-only ocsforge
